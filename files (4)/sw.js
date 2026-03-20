5c1816c4c246ba0a72cd4fd6ada5971ae7155d78
Horizon1590 <150209172+Horizon1590@users.noreply.github.com>
2026-03-19 22:23:08 -0600

    Add files via upload

---
+++ files (4)/sw.js
@@ -0,0 +1,39 @@
+// Freedom App Service Worker
+const CACHE_NAME = 'freedom-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
